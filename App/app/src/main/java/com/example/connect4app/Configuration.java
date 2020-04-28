0dc5e5289ae55099075417591c8f588089a5e3fc
Navy22junda <navy22junda@gmail.com>
2020-04-28 17:19:35 +0200

    Afegit la interficie mes lletja

--- App/app/src/main/java/com/example/connect4app/Configuration.java
+++ App/app/src/main/java/com/example/connect4app/Configuration.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Configuration extends Activity {
+public class Configuration extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+
     }
 }
