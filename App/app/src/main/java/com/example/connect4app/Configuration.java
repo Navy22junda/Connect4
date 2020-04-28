e9396c9e4af902a52f6a3166f9b8e5c3e7c08eed
Navy22junda <navy22junda@gmail.com>
2020-04-28 16:51:06 +0200

    Fent la connexio entre activities

---
+++ App/app/src/main/java/com/example/connect4app/Configuration.java
@@ -0,0 +1,15 @@
+package com.example.connect4app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Configuration extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
--- App/app/src/main/java/com/example/connect4app/MainActivity.java
+++ App/app/src/main/java/com/example/connect4app/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.connect4app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.buttonStart);
+        Button help = findViewById(R.id.buttonHelp);
+        Button exit = findViewById(R.id.buttonExit);
+
+        start.setOnClickListener(this);
+        help.setOnClickListener(this);
+        exit.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent;
+
+        switch (v.getId()){
+
+            case R.id.buttonStart:
+                intent = new Intent(this, Configuration.class);
+                startActivity(intent);
+                break;
+
+            case R.id.buttonHelp:
+                intent = new Intent(this, Help.class);
+                startActivity(intent);
+                break;
+
+            case R.id.buttonExit:
+                this.onDestroy();
+                break;
+        }
+
     }
 }
